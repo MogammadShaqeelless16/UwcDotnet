b9341a7d63d4853f327ab377cda8a7aa212e40a5
MogammadShaqeelless16 <shaqeeless4@gmail.com>
2020-09-01 11:33:40 -0700

    updates

---
+++ Maths/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(64));
+
+            Console.WriteLine(Math.Abs(-4.7));
+
+
+            Console.WriteLine(Math.Max(5, 10));
+
+            Console.WriteLine(Math.Min(5, 10));
+
+            Console.WriteLine(Math.Round(9.99));
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
+
+            string txts = "Hello World";
+            Console.WriteLine(txts.ToUpper());   // Outputs "HELLO WORLD"
+            Console.WriteLine(txts.ToLower());   // Outputs "hello world"
+
+            string firstName = "John ";
+            string lastName = "Doe";
+            string name = firstName + lastName;
+            Console.WriteLine(name);
+
+            string firstNames = "John ";
+            string lastNames = "Doe";
+            string names = string.Concat(firstNames, lastNames);
+            Console.WriteLine(names);
+
+            string firstNam = "John";
+            string lastNam = "Doe";
+            string nam = $"My full name is: {firstNam} {lastNam}";
+            Console.WriteLine(nam);
+        }
+    }
+}
