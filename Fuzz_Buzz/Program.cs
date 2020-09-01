ec2a50d2c5da4190e8dfab1c4e88d4a5b5e4ce42
MogammadShaqeelless16 <shaqeeless4@gmail.com>
2020-09-01 07:47:41 -0700

    Next project

---
+++ Fuzz_Buzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fuzz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("ApplesPears");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Apples");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Pear");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
