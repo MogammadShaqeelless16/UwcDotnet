eb37e857c00b367040316d9f471db0331915c50d
MogammadShaqeelless16 <shaqeeless4@gmail.com>
2020-09-15 00:56:18 -0700

    Completed assigment 2

---
+++ MSP_Assignment_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MSP_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 99;
+            while (i != 0)
+            {
+                Console.WriteLine(i + " Bottles of beer on the wall," + i + " Bottles of beer");
+                i--;
+                Console.WriteLine("Take one down and pass it around," + i + " Bottles of beer on the wall");
+            }
+        }
+    }
+}
---
+++ check if even or odd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace check_if_even_or_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
